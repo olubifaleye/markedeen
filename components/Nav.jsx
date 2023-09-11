8d17f42e867fc376ba515bfb4cbc999f4e09b3bc
olubifaleye <eniolubiportfolio@gmail.com>
2023-09-11 21:00:05 +0100

    Fixed resizing for navbar desktop and mobile

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ const Nav = () => {
     <div className="header">
         <nav className="navbar">
             
-            <Link href="/" className={clicked ? "max-sm:hidden" : "flex"}>
+            <Link href="/" className={clicked ? "max-md:hidden" : "flex"}>
                 <Image 
                     src="/assets/images/logo.png"
                     alt="Markedeen Logo"
@@ -36,17 +36,17 @@ const Nav = () => {
                 />
             </Link>
 
-            <div onClick={handleClick} className="max-sm:fixed max-sm:top-7 max-sm:right-10 md:hidden max-sm:text-white">
+            <div onClick={handleClick} className="max-md:fixed max-md:top-7 max-md:right-10 md:hidden max-md:text-white">
                 {clicked ? (<FaTimes size={32} style={{color: '#1B4571'}}/>) : <FaBars size={32}/> }   
             </div>
 
-            <ul className={clicked ? "flex max-sm:fixed max-sm:left-0 max-sm:top-0 max-sm:flex-col max-sm:bg-primary-blue max-sm:w-3/4 max-sm:items-center max-sm:h-full max-sm:z-999 max-sm:transition: max-sm:duration-300" : "flex max-sm:fixed max-sm:-left-full max-sm:top-0 max-sm:flex-col max-sm:bg-primary-blue max-sm:w-3/4 max-sm:items-center max-sm:h-full max-sm:z-999 max-sm:transition: max-sm:duration-300"}>
+            <ul className={clicked ? "flex max-md:fixed max-md:left-0 max-md:top-0 max-md:flex-col max-md:bg-primary-blue max-md:w-3/4 max-md:items-center max-md:h-full max-md:z-999 max-md:transition: max-md:duration-300" : "flex max-md:fixed list-none max-md:-left-full max-md:flex-col max-md:bg-primary-blue max-md:items-center max-md:z-999 max-md:transition: max-md:duration-300"}>
 
-                <Link href="/" className="md:hidden">
+                <Link href="/" className="md:hidden mr-10 mt-10">
                     <Image 
                         src="/assets/images/logo.png"
                         alt="Markedeen Logo"
-                        width={150}
+                        width={300}
                         height={100}
                         priority= 'true'
                         className="object-contain"
@@ -55,37 +55,37 @@ const Nav = () => {
 
             
                 <li className="nav_item">
-                    <Link href="/" className="link_item hover:border-b-4">
+                    <Link href="/" className="link_item hover:border-b-4 max-md:hover:-mb-1">
                         Services
                     </Link>
                 </li> 
 
                 <li className="nav_item">
-                    <Link href="/" className="link_item hover:border-b-4">
+                    <Link href="/" className="link_item hover:border-b-4 max-md:hover:-mb-1">
                         Our Philosphy
                     </Link>
                 </li> 
 
                 <li className="nav_item">
-                    <Link href="/" className="link_item hover:border-b-4">
+                    <Link href="/" className="link_item hover:border-b-4 max-md:hover:-mb-1">
                         Online Presence
                     </Link>
                 </li>
 
                 <li className="nav_item">
-                    <Link href="/" className="link_item hover:border-b-4">
+                    <Link href="/" className="link_item hover:border-b-4 max-md:hover:-mb-1">
                         Outperform Rivals
                     </Link>
                 </li> 
 
                 <li className="nav_item">
-                    <Link href="/" className="link_item hover:border-b-4">
+                    <Link href="/" className="link_item hover:border-b-4 max-md:hover:-mb-1">
                         Process
                     </Link>
                 </li>
 
                 <li className="nav_item">
-                    <Link href="/" className="link_item max-sm:pt-12">
+                    <Link href="/" className="max-md:pt-12">
                         <button 
                             type="button" 
                             onClick={() => {}}
@@ -97,7 +97,7 @@ const Nav = () => {
                 </li>
 
                 <li className="nav_item">
-                    <Link href="/" className="md:pl-40 link_item max-sm:pt-44">
+                    <Link href="/" className="link_item lg:pl-4 max-md:pt-44">
                         Company Email
                     </Link>
                 </li>       
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,22 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '940px',
+      // => @media (min-width: 940px) { ... }
+
+      'lg': '1200px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         satoshi: ['Satoshi', 'sans-serif'],

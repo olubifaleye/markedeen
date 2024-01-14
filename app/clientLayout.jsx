2ef959305c61e9a03b90635edeae06f0948c80ea
olubifaleye <eniolubiportfolio@gmail.com>
2024-01-14 14:56:55 +0000

    Updated layout.jsx to include metadata

---
+++ app/clientLayout.jsx
@@ -0,0 +1,40 @@
+"use client"
+
+import '../styles/globals.css';
+import Nav from '@components/Nav';
+import NavSmooth from '@components/NavSmooth';
+import Footer from '@components/Footer';
+import ChatWidget from '@components/ChatWidget';
+
+import { useEffect } from 'react'
+import { usePathname, useSearchParams } from 'next/navigation'
+
+//Added in Nav component to layout higher order component
+const ClientLayout = ({ children }) => {
+
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+ 
+  useEffect(() => {
+    const url = `${pathname}`
+    // You can now use the current URL
+    // ...
+  }, [pathname, searchParams])
+
+  return (
+    <html lang='en'>
+
+        <body className='bg-tertiary-blue'>
+            <main className=''>
+                {pathname === '/' ? <NavSmooth /> : <Nav />}    
+                {children}
+                <ChatWidget />
+                <Footer />
+
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default ClientLayout
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,40 +1,19 @@
-"use client"
-
 import '../styles/globals.css';
-import Nav from '@components/Nav';
-import NavSmooth from '@components/NavSmooth';
-import Footer from '@components/Footer';
-import ChatWidget from '@components/ChatWidget';
-import { metadata } from './layoutMetadata';
+import ClientLayout from './clientLayout';
 
-import { useEffect } from 'react'
-import { usePathname, useSearchParams } from 'next/navigation'
-
-//Added in Nav component to layout higher order component
-const RootLayout = ({ children }) => {
-
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
- 
-  useEffect(() => {
-    const url = `${pathname}`
-    // You can now use the current URL
-    // ...
-  }, [pathname, searchParams])
-
-  return (
-    <html lang='en'>
-        <body className='bg-tertiary-blue'>
-            <main className=''>
-                {pathname === '/' ? <NavSmooth /> : <Nav />}    
-                {children}
-                <ChatWidget />
-                <Footer />
-
-            </main>
-        </body>
-    </html>
-  )
+// Set metadata for layout file
+export const metadata = {
+  title: "Markedeen",
+  description: "Markedeen goes beyond the realms of traditional marketing — we're your end-to-end business growth partner.",
+  keywords: [
+    "Markedeen",
+    "Growth",
+    "Partner",
+    "Marketing",
+    "Agency",
+  ]
 }
 
-export default RootLayout
+export default function RootLayout({ children }) {
+  return <ClientLayout>{children}</ClientLayout>;
+}
